4f3ff5e30499d1076fb74adce6c322c29887b040
daimaou92 <daimaou92@oulook.com>
2022-02-25 18:27:31 +0530

    feat: font configuration added

--- xconf.sh
+++ xconf.sh
@@ -21,12 +21,7 @@ S=`scriptDir`
 P=`pwd`
 
 # Font
-cd /tmp
-git clone https://aur.archlinux.org/nerd-fonts-hack.git
-cd nerd-fonts-hack
-makepkg -sirc --noconfirm
-cd ../ && rm -rf nerd-fonts-hack
-cd $P
+
 
 [ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="$HOME/.config"
 cd $S/x
@@ -42,7 +37,9 @@ ln -sf "$(pwd)/autostart/picom.desktop" \
 	"${XDG_CONFIG_HOME}/autostart/picom.desktop"
 
 
-# i3 config
+##################
+####### i3 #######
+##################
 mkdir -p "$XDG_CONFIG_HOME/i3"
 cd $S/x/i3 && \
 for FILE in * .[^.]*; do
@@ -52,7 +49,9 @@ for FILE in * .[^.]*; do
 	fi
 done
 
-# kitty config
+##################
+#### Kitty #######
+##################
 mkdir -p "$XDG_CONFIG_HOME/kitty"
 cd $S/x/kitty && \
 for FILE in * .[^.]*; do
@@ -62,13 +61,43 @@ for FILE in * .[^.]*; do
 	fi
 done
 
-# picom config
+##################
+#### Picom #######
+##################
 mkdir -p "$XDG_CONFIG_HOME/picom"
 cd $S/x/picom && \
 ln -sf "$(pwd)/picom.conf" "${XDG_CONFIG_HOME}/picom/picom.conf"
 
+#################
+#### Fonts ######
+#################
 
-# set wallpaper
+# Install Hack Nerd Patched Font
+cd /tmp
+git clone https://aur.archlinux.org/nerd-fonts-hack.git
+cd nerd-fonts-hack
+makepkg -sirc --noconfirm
+cd ../ && rm -rf nerd-fonts-hack
+cd $P
+
+# enable font config support
+sudo mkdir -p /etc/fonts/conf.d
+sudo ln -sf /usr/share/fontconfig/conf.avail/50-user.conf \
+	/etc/fonts/conf.d/
+sudo ln -sf /usr/share/fontconfig/conf.avail/51-local.conf \
+	/etc/fonts/conf.d/
+
+# enable sub-pixel rendering (user only)
+mkdir -p "$XDG_CONFIG_HOME/fontconfig/conf.d"
+ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf \
+	$XDG_CONFIG_HOME/fontconfig/conf.d/
+
+# custom font config
+ln -sf $S/x/fontconfig/fonts.conf "$XDG_CONFIG_HOME/fontconfig/"
+
+#######################
+##### Wallpaper #######
+#######################
 mkdir -p "$HOME/.cache" && cp "${SD}/wallpaper.jpg" \
 	"$HOME/.cache/.wallpaper.jpg"
 feh --bg-fill "$HOME/.cache/.wallpaper.jpg"
